d1dd2eb03523db09c40f2ca3b87b515319968921
oflo03 <samleebw@naver.com>
2023-01-02 00:57:44 +0900

    2D 렌더링과 애니메이션 연습

--- pico2dexample/pico2dexample.py
+++ pico2dexample/pico2dexample.py
@@ -1,18 +1,23 @@
+from pico2d import *
 
-from pico2d import*
+open_canvas(960,544)
 
-open_canvas()
+background = load_image('background.png')
+player = load_image('player.png')
 
-grass = load_image('grass.png')
-character = load_image('character.png')
+xframe = 100
+yframe = 55
+size = 3
+x = 0
+cnt = 0
 
-x=0
-while(x<800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x +=2
-    delay(0.01)
+while (x < 960):
+    clear_canvas()
+    background.draw_now(480, 272)
+    player.clip_draw(xframe * (cnt + 8 if cnt < 2 else cnt - 2), yframe * (8 if cnt < 2 else 7), xframe, yframe, x, 140,xframe * size, yframe * size)
+    update_canvas()
+    cnt = (cnt + 1) % 10
+    x += 20
+    delay(0.08)
 
 close_canvas()
-    
