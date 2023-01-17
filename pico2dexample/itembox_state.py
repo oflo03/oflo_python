532ae65d9856e4ec0e905ffe5a4a7c0b93b64d17
oflo03 <samleebw@naver.com>
2023-01-18 03:06:32 +0900

    실습 5
    learn the gameframework
    logo, title, item state 구현

---
+++ pico2dexample/itembox_state.py
@@ -0,0 +1,40 @@
+from pico2d import *
+import game_framework
+import  play_state
+
+running = True
+image = None
+
+def enter():
+    global image
+    image = load_image('item_state.png')
+
+def exit():
+    global image
+    del image
+
+def update():
+    pass
+
+def draw():
+    global image
+    image.draw(400, 300)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.pop_state()
+            elif event.key == SDLK_0:
+                play_state.boy.item_image = None
+                game_framework.pop_state()
+            elif event.key == SDLK_1:
+                play_state.boy.item_image = load_image('ball21X21.png')
+                game_framework.pop_state()
+            elif event.key == SDLK_2:
+                play_state.boy.item_image = load_image('ball41X41.png')
+                game_framework.pop_state()
---
+++ pico2dexample/main.py
@@ -0,0 +1,7 @@
+import pico2d
+import logo_state
+import game_framework
+
+pico2d.open_canvas()
+game_framework.run(logo_state)
+pico2d.close_canvas()
